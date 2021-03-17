ca1207563d8fa9d93bc6d6de3c162a89d83e6143
RamonGiovane <ramon.giovane@gmail.com>
2021-03-17 15:59:58 -0300

    Tela de login

--- SuperDiaEstoqueSE/src/br/com/superdia/SuperDiaEstoque.java
+++ SuperDiaEstoqueSE/src/br/com/superdia/SuperDiaEstoque.java
@@ -1,14 +1,17 @@
 package br.com.superdia;
 
 import br.com.superdia.controller.ProductController;
+import br.com.superdia.controller.UserController;
 import br.com.superdia.ui.UILogin;
 
 public class SuperDiaEstoque {
 
-	ProductController controller;
+	ProductController productController;
+	UserController userController;
 	
 	public SuperDiaEstoque() {
-		controller = new ProductController();
+		productController = new ProductController();
+		userController = new UserController();
 	}
 	
 	public static void main(String[] args) {
@@ -18,7 +21,7 @@ public class SuperDiaEstoque {
 
 	private void iniciar() {
 		
-		new UILogin();
+		new UILogin(userController, productController);
 		//new UIPrincipal(controller);
 		
 	}
---
+++ SuperDiaEstoqueSE/src/br/com/superdia/controller/User.java
@@ -0,0 +1,41 @@
+package br.com.superdia.controller;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private String profile;
+	
+	public User() {
+		
+	}
+	
+	public User(String username, String password, String profile) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.profile = profile;
+	}
+
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+	
+}
