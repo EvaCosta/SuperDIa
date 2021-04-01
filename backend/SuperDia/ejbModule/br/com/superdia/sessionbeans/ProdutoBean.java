e53ca078b4159856587db5d8d9a31e799da475b1
RamonGiovane <ramon.giovane@gmail.com>
2021-04-01 20:42:16 -0300

    API para importar produtos externos

--- backend/SuperDia/ejbModule/br/com/superdia/sessionbeans/ProdutoBean.java
+++ backend/SuperDia/ejbModule/br/com/superdia/sessionbeans/ProdutoBean.java
@@ -1,6 +1,10 @@
 package br.com.superdia.sessionbeans;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
@@ -8,11 +12,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaQuery;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.superdia.dto.ProdutosExternos;
 import br.com.superdia.modelo.Produto;
 
 @Stateless
 @Remote(IProduto.class)
-public class ProdutoBean implements IProduto{
+public class ProdutoBean implements IProduto, IProdutoExterno{
 	
 	@PersistenceContext(unitName = "SuperDia")
 	EntityManager em;
@@ -60,6 +67,40 @@ public class ProdutoBean implements IProduto{
 	@Override
 	public Produto buscaPorId(Long id) {
 		return em.find(Produto.class, id);
+	}
+
+	@Override
+	public boolean importarProdutosExternos(Loja loja) {
+		
+
+		URL url = new URL(loja.getLink());
+		InputStream in = url.openStream();
+	
+		String json = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+		
+		ObjectMapper mapper = new ObjectMapper();
+
+		ProdutosExternos produtosExternos =
+				mapper.readValue(json, ProdutosExternos.class);
+		
+		
+		List<Produto> produtos = produtosExternos.products.stream().map(p -> new Produto(p.title,
+				p.product_type, p.variants.get(0).price, loja.getId(), p.id)).collect(Collectors.toList());
+		
+		for (Produto produto : produtos) {
+			
+			try {
+			  TypedQuery<Produto> query = em.createQuery("from Produto WHERE lojaId=? and idExterno=?", Produto.class);
+			  query.setParameter(1, produto.getIdExterno());
+			  query.setParameter(2, produto.getIdExterno());
+			  query.getSingleResult();
+			}catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
+			
+			adiciona(produto);
+		}
 	}
 	
 }
---
+++ backend/SuperDiaClient/ejbModule/br/com/superdia/sessionbeans/IProdutoExterno.java
@@ -0,0 +1,8 @@
+package br.com.superdia.sessionbeans;
+
+import br.com.superdia.modelo.Loja;
+
+public interface IProdutoExterno {
+	
+	public boolean importarProdutosExternos(Loja loja);
+}
