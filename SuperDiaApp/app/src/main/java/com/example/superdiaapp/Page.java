7eaf7d01ed4ae69c4fd0a28cec1101fff6a60fb3
EvaCosta <evacosta.if@gmail.com>
2021-04-03 16:18:53 -0300

    Create Page.java

---
+++ SuperDiaApp/app/src/main/java/com/example/superdiaapp/Page.java
@@ -0,0 +1,36 @@
+package com.example.superdiaapp;
+
+public class Page {
+    private String nome, descricao;
+    private Double preco;
+
+    public Page(String nome, String descricao, Double preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
